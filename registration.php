fd225ff2420befe6423a20a2979f1624ec65ccac
nertal23 <nertilalika@yahoo.it>
2016-09-23 13:45:40 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'W3D_ExportOrders',
+    'W3D_Exportorders',
     __DIR__
 );
