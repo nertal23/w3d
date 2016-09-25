bfa76a8a01de235880deea141480403c76346636
nertal23 <nertilalika@yahoo.it>
2016-09-25 22:15:25 +0200

    Update Array2XML.php

--- Model/Export/Adapter/Array2XML.php
+++ Model/Export/Adapter/Array2XML.php
@@ -44,7 +44,7 @@
  *
 **/
 
-namespace W3development\ExportOrders\Model\Export\Adapter;
+namespace W3development\Exportorders\Model\Export\Adapter;
 
 class Array2XML extends \Magento\Framework\Model\AbstractModel
 {
